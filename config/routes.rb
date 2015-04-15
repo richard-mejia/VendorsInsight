d1c1c3b0cbb3c196cf497f28b0d6b77a3c67d201
santivasquez <santivaz852@hotmail.com>
2015-04-14 19:50:03 -0500

    error salir solicituds

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   resources :solicituds
-
   resources :usuarios
+  get    'index' => 'login#index'
   get    'login'   => 'login#autenticar'
   post   'login'   => 'login#create'
-  delete 'logout'  => 'login#destroy'
-  resources :login
+  get 'logout'  => 'login#destroy'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

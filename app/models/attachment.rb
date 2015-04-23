bed69900192d38c5de82ffe4bb9fcc8064a86d52
santivasquez <santivaz852@hotmail.com>
2015-04-22 19:02:15 -0500

    previniendonos de los malos

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -2,7 +2,7 @@ class Attachment < ActiveRecord::Base
   has_attached_file :archivo,
   :storage => :fog,
   :fog_credentials => {provider: 'AWS', 
-    aws_access_key_id: 'AKIAI5HBYPNTFOMEDEJA', 
-    aws__secret_access_key: '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'},
+    aws_access_key_id: '', 
+    aws__secret_access_key: ''},
   :fog_directory => "Nombre de tu bucket"
 end

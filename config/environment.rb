a20007c13db912d6fa8788183530f7eabe139293
santivasquez <santivaz852@hotmail.com>
2015-04-24 11:32:51 -0500

    seguridad contra hackers

--- config/environment.rb
+++ config/environment.rb
@@ -4,14 +4,14 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 # Don't care if the mailer can't send.
- # config.action_mailer.raise_delivery_errors = true
-# Propiedad config = Propiedad.first
-# ActionMailer::Base.smtp_settings = {
- # :user_name => config.id_correo,
-  #:password => config.pass_correo,
- # :domain => 'heroku.com',
- # :address => 'smtp.sendgrid.net',
- # :port => 587,
- # :authentication => :plain,
- # :enable_starttls_auto => true
-#}
+ 
+Propiedad config = Propiedad.first
+ ActionMailer::Base.smtp_settings = {
+  :user_name => config.id_correo,
+  :password => config.pass_correo,
+  :domain => 'heroku.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}

e58f952eb9b14d6b8b7b9a2f2d56e2add02609ff
santivasquez <santivaz852@hotmail.com>
2015-04-24 11:04:48 -0500

    correo desativado temporalmente

--- config/environment.rb
+++ config/environment.rb
@@ -5,13 +5,13 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 # Don't care if the mailer can't send.
  # config.action_mailer.raise_delivery_errors = true
-Propiedad config = Propiedad.first
- ActionMailer::Base.smtp_settings = {
-  :user_name => config.id_correo,
-  :password => config.pass_correo,
-  :domain => 'heroku.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
+# Propiedad config = Propiedad.first
+# ActionMailer::Base.smtp_settings = {
+ # :user_name => config.id_correo,
+  #:password => config.pass_correo,
+ # :domain => 'heroku.com',
+ # :address => 'smtp.sendgrid.net',
+ # :port => 587,
+ # :authentication => :plain,
+ # :enable_starttls_auto => true
 }

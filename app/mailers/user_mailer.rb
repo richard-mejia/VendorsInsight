c1b508f0b00dee897ab64f8cc5dc87162ea42cc9
santivasquez <santivaz852@hotmail.com>
2015-05-06 13:38:30 -0500

    prueba 4

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,12 @@
 class UserMailer < ApplicationMailer
-    default from: 'vendorsInsight@aplication.com'
+    default from: 'from@example.com'
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.user_mailer.confirmar_solicitud.subject
   #
   def solicitud_creada(usuario_correo)
-    @greeting = "Hola!"
+    @greeting = "Hola"
     mail(to: usuario_correo, subject: 'solicitud creada')
-     # mail(to: 'sebaslv12@hotmail.com', subject: 'peticion de solicitud')
   end
 end
